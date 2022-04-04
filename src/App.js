aef83be1721197f4d1ce7440462e2b6ec16b462b
sukanya2712 <sukanyanaik511@gmail.com>
2022-04-04 10:08:56 +0530

    reactdoc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    
+    const [calc,setCalc]=useState("");
+    const [result,setResult]=useState("");
+
+    const ops=['/','*','+','.',];
+
+    const updateCalc= value =>{
+        if(
+            ops.includes(value) && calc === '' ||
+            ops.includes(value) && ops.includes(calc.slice(-1))
+        ){
+            return;
+        }
+
+        if(!ops.includes(value)){
+            setResult(eval(calc+value).toString());
+        }
+        setCalc(calc + value);
+    }
+
+    const calcu=()=>
+    {
+        setCalc(eval(calc).toString());
+    }
+
+
+    const deletelast=() =>{
+        if(calc === ''){
+            return;
+        }
+
+        const value =calc.slice(0,-1);
+        setCalc(value);
+    }
+    const createDigits=()=>{
+        const digits=[];
+
+        for(let i=1;i<10;i++){
+
+            digits.push(
+                <button  onClick={()=> updateCalc(i.toString())}
+                key={i}>{i}</button>
+            )
+        }
+
+        return digits;
+    }
+
+
+    return(
+        <div className="App">
+            <div className="calculator">
+                <div className="display">
+                     {result ? <span>({result})</span> : ' '}
+                     {calc || "0"}
+                </div>
+           
+            
+                <div className="operators">
+                <buttons onClick={()=>updateCalc("/")}> /</buttons>
+                <buttons onClick={()=>updateCalc("*")}>*</buttons>
+                <buttons onClick={()=>updateCalc("+")}>+</buttons>
+                <buttons onClick={()=>updateCalc("-")}>-</buttons>
+
+                <buttons onClick={deletelast}>DEL</buttons>
+
+                </div>
+
+                <div className="digits">
+                {createDigits()}
+                </div>
+                <div className="digi">
+                <buttons onClick={()=>updateCalc("0") }>0</buttons>
+                <buttons onClick={()=>updateCalc(".")}>.</buttons>
+                <buttons onClick={calcu}>=</buttons>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
